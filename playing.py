71181482982994af029466b6691bd7f13aab2891
nico <nicomarruccelli@live.com>
2022-08-31 22:12:14 -0300

    alltogheterwellformed

--- playing.py
+++ playing.py
@@ -1,8 +1,13 @@
+
+#Just getting the actual time.
 import datetime
+from turtle import title
 e = datetime.datetime.now()
-print ("Current date and time = %s" % e)
-print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
-print ("The time shall be now: = %s:%s:%s" % (e.hour, e.minute, e.second))
+#Asking user's name, and printing name + date + time and remove WS and Capt.
+name = input("Whats ur name pal? ").title().strip()
+print ("Hi " + name + ", how was your day?" + ". ", end="")
+print ("Today's date and time is: %s/%s/%s." % (e.day, e.month, e.year)," %s:%s:%s" % (e.hour, e.minute, e.second))
+
 
 
 
